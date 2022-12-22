3dc4d9ed487576761c54ab7428d710c2c9d3fa56
Gursartaj Singh Grewal <119475074+GSGhac@users.noreply.github.com>
2022-12-22 19:44:21 +0530

    Add files via upload

---
+++ Outout to assignment 1 and 2.py
@@ -0,0 +1,88 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: D:\1st Semester 2022\Coding\assignment 1 q1.py ==================
+53.33333333333333
+
+=================== RESTART: D:\1st Semester 2022\Coding\assignment 1 q2.py ==================
+Please enter gross input: 10000
+Please enter number of dependents: 4
+Income tax amount to $-2400.0
+
+================= RESTART: D:\1st Semester 2022\Coding\assignment 1 ques 3.py ================
+Please enter number of seconds:75
+Duration of 75 is 1mins and 15secs
+
+=================== RESTART: D:\1st Semester 2022\Coding\assignment 1 q4.py ==================
+The final output is75
+
+================= RESTART: D:\1st Semester 2022\Coding\assignment 1 ques 5.py ================
+sin(0)=0.0000 and cos(0)=1.0000
+sin(15)=0.2588 and cos(15)=0.9659
+sin(30)=0.5000 and cos(30)=0.8660
+sin(45)=0.7071 and cos(45)=0.7071
+sin(60)=0.8660 and cos(60)=0.5000
+sin(75)=0.9659 and cos(75)=0.2588
+sin(90)=1.0000 and cos(90)=0.0000
+sin(105)=0.9659 and cos(105)=-0.2588
+sin(120)=0.8660 and cos(120)=-0.5000
+sin(135)=0.7071 and cos(135)=-0.7071
+sin(150)=0.5000 and cos(150)=-0.8660
+sin(165)=0.2588 and cos(165)=-0.9659
+sin(180)=0.0000 and cos(180)=-1.0000
+sin(195)=-0.2588 and cos(195)=-0.9659
+sin(210)=-0.5000 and cos(210)=-0.8660
+sin(225)=-0.7071 and cos(225)=-0.7071
+sin(240)=-0.8660 and cos(240)=-0.5000
+sin(255)=-0.9659 and cos(255)=-0.2588
+sin(270)=-1.0000 and cos(270)=-0.0000
+sin(285)=-0.9659 and cos(285)=0.2588
+sin(300)=-0.8660 and cos(300)=0.5000
+sin(315)=-0.7071 and cos(315)=0.7071
+sin(330)=-0.5000 and cos(330)=0.8660
+sin(345)=-0.2588 and cos(345)=0.9659
+>>> 
+===================== RESTART: D:\1st Semester 2022\Coding\Hello world.py ====================
+Hello World
+>>> 
+============== RESTART: D:\1st Semester 2022\Coding\Largest of three numbers.py ==============
+Enter first number:25
+Enter second number:35
+Enter third number:1198756
+1198756 is the largest of three numbers
+>>> 
+====================== RESTART: D:\1st Semester 2022\Coding\Q1_assgn2.py =====================
+length of string= 35
+egaugnal evitisnes esac a si nohtyp
+a case sensitive
+python is object oriented language
+10
+pythonisacasesensitivelanguage
+>>> 
+====================== RESTART: D:\1st Semester 2022\Coding\Q2_assgn2.py =====================
+Hey, ABC Here!
+My SID is 2210XXXX 
+I am from XYZ department and my CGPA is 9.9
+>>> 
+====================== RESTART: D:\1st Semester 2022\Coding\Q3_assgn2.py =====================
+a & b = 8
+a | b = 58
+a ^ b = 50
+Left shifting both a and b with 2 bits = 224 = 40
+Right shifting a with 2 bits and b with 4 bits = 14 = 160
+>>> 
+====================== RESTART: D:\1st Semester 2022\Coding\Q4_assgn2.py =====================
+Enter first number: 55
+Enter second number: 65
+Enter third number: 75
+The greatest number of the three is 75.0
+>>> 
+====================== RESTART: D:\1st Semester 2022\Coding\Q5_assgn2.py =====================
+Enter the string: sartaj
+No
+>>> 
+====================== RESTART: D:\1st Semester 2022\Coding\Q6_assgn2.py =====================
+Enter length of side 1: 50
+Enter length of side 2: 35
+Enter length of side 3: 47
+Yes
---
+++ assignment 3 solutions.py
@@ -0,0 +1,106 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: D:\1st Semester 2022\Coding\q1_assgn3.py ==================
+Enter the string:abcd
+
+Dictionary containing 'Letter':'Letter Count' is shown below:
+{'b': 1, 'a': 1, 'c': 1, 'd': 1}
+
+=================== RESTART: D:\1st Semester 2022\Coding\q2_assgn3.py ==================
+Enter Date below to get date of next day.
+
+Enter Day [1-31]:25
+Enter Month [1-12]:3
+Enter Year [1800-2025]:2004
+
+Next Date in format Day/Month/Year is: 26/3/2004
+
+=================== RESTART: D:\1st Semester 2022\Coding\q3_assgn3.py ==================
+Enter the numbers separated by space:10
+
+List containing (n,n^2) is shown below:
+[(10, 100)]
+
+=================== RESTART: D:\1st Semester 2022\Coding\q4_assgn3.py ==================
+Enter grade points:100
+
+Error
+Please enter grade in range [4,10]
+
+=================== RESTART: D:\1st Semester 2022\Coding\q4_assgn3.py ==================
+Enter grade points:5
+Your grade is 'C' and Below Average performance.
+
+=================== RESTART: D:\1st Semester 2022\Coding\q5_assgn3.py ==================
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+============ RESTART: D:\1st Semester 2022\Coding\Assignment 3 question 6.py ===========
+
+Question 6
+Enter student name:Gursartaj
+Enter student SID:221030004
+Enter Y to continue or N to end:Y
+Enter student name:Gursartaj
+Enter student SID:22103004
+Enter Y to continue or N to end:N
+
+Q.6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{221030004: 'Gursartaj', 22103004: 'Gursartaj'}
+
+Q.6(b)
+The Dictionary after sorting according to name:
+{22103004: 'Gursartaj'}
+
+Q.6(c)
+The Dictionary after sorting according to SID:
+{22103004: 'Gursartaj', 221030004: 'Gursartaj'}
+
+Q.6(d)
+Enter SID to get name of student:22103004
+Name of student with SID 22103004 is Gursartaj.
+>>> 
+============ RESTART: D:\1st Semester 2022\Coding\Assignment 3 question 7.py ===========
+Enter number of elements N in fibonacci series:
+[N must be positive Integer]: N=7
+
+The fibonacci series with 7 elements is shown below:
+[1, 1, 2, 3, 5, 8, 13]
+
+Average of given fibonacci series is 4.71
+>>> 
+============ RESTART: D:\1st Semester 2022\Coding\Assignment 3 question 8.py ===========
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Q.8(a)
+
+A new Set of all 'elements that are in Set1 and Set2 but not both' is:
+{1, 3, 5, 6, 8}
+
+Q.8(b)
+
+A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is:
+{3, 6, 8, 9, 13, 17}
+
+Q.8(c)
+
+A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is:
+{1, 2, 4, 5}
+
+Q.8(d)
+
+A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is:
+{6, 7, 8, 9, 10}
+
+Q.8(e)
+
+A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3.
+{10, 7}
